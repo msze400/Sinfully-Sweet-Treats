4a916193390d7436c75f6116bd2d5eb913359246
msze400 <msze400@gmail.com>
2022-05-26 18:05:15 -0400

    added grid feature to minibio

--- src/components/MiniBio/MiniBio.tsx
+++ src/components/MiniBio/MiniBio.tsx
@@ -4,17 +4,19 @@ import React, { FC } from 'react';
 interface MiniBioProps {}
 
 const MiniBio: FC<MiniBioProps> = () => (
-<div className="grid grid-cols-4 minibio">
-  <div className="col-span-2">
-    <h1 className='text-4xl text-right pr-9'>
-      Home made New Jersey Cottage Law, 
-      <br></br> for Pickup
+<div className="grid grid-cols-6 minibio mt-8">
+  <div className="col-span-2 col-start-2">
+    <h1 className='text-4xl text-left'>
+      Home Made New Jersey Cottage Desserts 
+     for Any Occasion
     </h1>
     <br></br>
-    <h2>Sinfully Sweet treats is dedicated to providing friends and family with a great selection of sweets and treats. Perfect for catered events to kiddies birthday parties! </h2>
     </div>
-  <div>Made With Love</div>
+  <div className='text-center'>Made With Love</div>
   <div>And Care</div>
+    <div className='col-start-2 col-span-2 text-lg'>
+      Sinfully Sweet treats is dedicated to providing friends and family with a great selection of sweets and treats. Perfect for catered events to kiddies birthday parties! 
+      </div>
 </div>
 );
 
