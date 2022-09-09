812fad94d262ac439c0c4aaab9dea98f7cee3cf0
msze400 <msze400@gmail.com>
2022-09-09 12:25:46 -0400

    Fixed Mobile Background

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -7,7 +7,9 @@ import NotificationBanner from '../NotificationBanner/NotificationBanner'
 
 interface HomePageProps {}
 
-const logo = require('../../assets/SinfullySweetTreats_v2.svg').default
+// const logo = require('../../assets/SinfullySweetTreats_v2.svg').default
+const logo = require('../../assets/SS_Logo_White.svg').default
+
 const BG = require('../../assets/HomePageBG_v3.png')
 
 const HomePage: FC<HomePageProps> = () => {
@@ -15,16 +17,10 @@ const HomePage: FC<HomePageProps> = () => {
     return (
         <>
             <NotificationBanner />
-            <div
-                className="grid grid-cols-10 bg-fixed bg-center bg-cover bg-no-repeat relative items-center  "
-                style={{
-                    backgroundImage: `url(${BG})`,
-                    height: '85vh',
-                }}
-            >
+            <div className="grid grid-cols-10 bg-fixed bg-center bg-cover bg-no-repeat relative items-center homepage ">
                 <img
-                    className="object-cover col-span-8 col-start-2 sm:col-start-3 md:col-start-7 h-1/2 sm:3/4 md:h-5/6 animate__animated animate__zoomIn"
-                    src={logo}
+                    className="object-cover col-span-8 col-start-2 sm:col-start-3 md:col-start-7 h-1/2 sm:3/4 md:h-5/6 animate__animated animate__zoomIn logo"
+                    // src={logo}
                     alt="white-logo"
                 ></img>
             </div>
